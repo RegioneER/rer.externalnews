cbc944562deb80e1b1cf7abcbd32db2ff3178a28
Andrea Cecchi <andrea.cecchi85@gmail.com>
2018-01-05 09:48:59 +0100

    Back to development: 0.1.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='rer.externalnews',
-    version='0.1.2',
+    version='0.1.3.dev0',
     description="A Plone add-on for Regione Emilia Romagna",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
