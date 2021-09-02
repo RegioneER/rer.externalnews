27afa57da6aa10c934acac41cde6a5f25348e77f
Daniele Andreotti <daniele.andreotti@redturtle.it>
2021-09-02 14:35:21 +0200

    Back to development: 1.0.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='rer.externalnews',
-    version='1.0.2',
+    version='1.0.3.dev0',
     description="A Plone add-on for Regione Emilia Romagna",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
