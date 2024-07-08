1473e73c74c1f0436fc0384d3732c541b04740c3
Andrea Cecchi <andrea.cecchi85@gmail.com>
2024-07-08 16:15:59 +0200

    fix pyroma

--- setup.py
+++ setup.py
@@ -5,20 +5,23 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-long_description = '\n\n'.join([
-    open('README.rst').read(),
-    open('CONTRIBUTORS.rst').read(),
-    open('CHANGES.rst').read(),
-])
+long_description = "\n\n".join(
+    [
+        open("README.rst").read(),
+        open("CONTRIBUTORS.rst").read(),
+        open("CHANGES.rst").read(),
+    ]
+)
 
 
 setup(
-    name='rer.externalnews',
-    version='1.1.1.dev0',
+    name="rer.externalnews",
+    version="1.1.1.dev0",
     description="A Plone add-on for Regione Emilia Romagna",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 5.0",
@@ -27,31 +30,31 @@ setup(
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
-    keywords='Python Plone',
-    author='RedTurtle Technology',
-    author_email='sviluppoplone@redturtle.it',
-    url='https://pypi.python.org/pypi/rer.externalnews',
-    license='GPL version 2',
-    packages=find_packages('src', exclude=['ez_setup']),
-    namespace_packages=['rer'],
-    package_dir={'': 'src'},
+    keywords="Python Plone",
+    author="RedTurtle Technology",
+    author_email="sviluppoplone@redturtle.it",
+    url="https://pypi.python.org/pypi/rer.externalnews",
+    license="GPL version 2",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    namespace_packages=["rer"],
+    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'plone.api',
-        'Products.GenericSetup>=1.8.2',
-        'setuptools',
-        'plone.app.dexterity',
+        "plone.api",
+        "Products.GenericSetup>=1.8.2",
+        "setuptools",
+        "plone.app.dexterity",
     ],
     extras_require={
-        'test': [
-            'plone.app.testing',
+        "test": [
+            "plone.app.testing",
             # Plone KGS does not use this version, because it would break
             # Remove if your package shall be part of coredev.
             # plone_coredev tests as of 2016-04-01.
-            'plone.testing>=5.0.0',
-            'plone.app.contenttypes',
-            'plone.app.robotframework[debug]',
+            "plone.testing>=5.0.0",
+            "plone.app.contenttypes",
+            "plone.app.robotframework[debug]",
         ],
     },
     entry_points="""
